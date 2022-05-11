75991090a380cdff5f557f08b71aa9a689cbd348
marcoestrada-17 <javier.itza.est@qroo.nuevaescuela.mx>
2022-05-11 18:18:42 -0500

    Craecion de Column

--- app/src/main/java/com/marco/mymoviesmarco/MainActivityMarcoItza.kt
+++ app/src/main/java/com/marco/mymoviesmarco/MainActivityMarcoItza.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -47,12 +46,20 @@ class MainActivity : ComponentActivity() {
 fun DefaultPreview() {
     MoviesMarcoItzaTheme {
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+
         ){
-                Greeting(
+            Greeting(
                     name= "Marco", modifier = Modifier
-                        .background(Color.LightGray))
-                Greeting("Android")
+                    .background(Color.LightGray)
+                    .weight (2f)
+            )
+            Greeting(
+                name= "Android", modifier = Modifier
+                    .background(Color.Yellow)
+                    .weight (1f)
+            )
             }
     }
 }
