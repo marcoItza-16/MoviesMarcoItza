39fdcaba05f690b253aeddc72e2885b885de041b
marcoestrada-17 <javier.itza.est@qroo.nuevaescuela.mx>
2022-05-12 00:32:36 -0500

    Craecion de Text

--- app/src/main/java/com/marco/mymoviesmarco/MainActivityMarcoItza.kt
+++ app/src/main/java/com/marco/mymoviesmarco/MainActivityMarcoItza.kt
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
@@ -16,7 +17,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.marco.mymoviesmarco.ui.theme.MoviesMarcoItzaTheme
@@ -27,11 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MoviesMarcoItzaTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
                 }
             }
         }
@@ -47,37 +49,43 @@ fun MediaItem() {
                 .height(200.dp)
                 .fillMaxWidth()
                 .background(color = Color.Red)
+
             ) {
         }
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colors.secondary)
+                    .padding(16.dp)
+            ) {
             Text("Title 1")
         }
 }
 
 @Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier
-) {
-
-    Text(text = "Hello $name!", modifier = modifier)
+fun Greeting( name: String, modifier: Modifier = Modifier) {
+       Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
-@Preview(showBackground = true, widthDp = 200, heightDp = 100)
+//@Preview(showBackground = true, widthDp = 200, heightDp = 100)
 @Composable
 fun ButtonText() {
     Box(modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Text(
-        text = "Hello Marco",
-        modifier = Modifier
-            .background(Color.Cyan)
-            .border(width =  2.dp, color = Color.Blue)
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-            .background(Color.LightGray)
-            .border(width =  2.dp, color = Color.Red)
-            .padding(8.dp)
-
+        text = "Hello World",
+        style = MaterialTheme.typography.h4.copy(
+            shadow = Shadow(
+                offset = Offset(5f, 5f),
+                blurRadius = 5f,
+                color = Color.Black.copy(alpha = 0.5f)
+            )
+        )
 
         )
     }
@@ -87,3 +95,4 @@ fun ButtonText() {
 @Composable
 fun DefaultPreview() {
     }
+}
