5b540ffe5ea68a66421c1d65852d69c8b35ed68c
marcoestrada-17 <javier.itza.est@qroo.nuevaescuela.mx>
2022-05-12 10:33:26 -0500

    Craecion la Cargar de Imagen

--- app/src/main/java/com/marco/mymoviesmarco/MainActivityMarcoItza.kt
+++ app/src/main/java/com/marco/mymoviesmarco/MainActivityMarcoItza.kt
@@ -5,14 +5,15 @@ import android.os.Bundle
 import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.outlined.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,11 +21,15 @@ import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.translationMatrix
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import com.marco.mymoviesmarco.ui.theme.MoviesMarcoItzaTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,13 +39,15 @@ class MainActivity : ComponentActivity() {
             MoviesMarcoItzaTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
+                    MediaItem()
+
                 }
             }
         }
     }
 }
 
-//@Preview(showBackground = true)
+@Preview(showBackground = true)
 @Composable
 fun MediaItem() {
     Column {
@@ -48,9 +55,16 @@ fun MediaItem() {
             modifier = Modifier
                 .height(200.dp)
                 .fillMaxWidth()
-                .background(color = Color.Red)
-
             ) {
+            Image(
+                painter = rememberImagePainter(
+                    data = "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/480/public/media/image/2019/03/lenguajes_programacion_odiados_amados_2019.jpg?itok=N85E5HTT" ,
+            ),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+            )
+
         }
             Box(
                 contentAlignment = Alignment.Center,
@@ -59,7 +73,11 @@ fun MediaItem() {
                     .background(MaterialTheme.colors.secondary)
                     .padding(16.dp)
             ) {
-            Text("Title 1")
+            Text(
+                text ="Title 1",
+                style = MaterialTheme.typography.h6
+            )
+
         }
 }
 
